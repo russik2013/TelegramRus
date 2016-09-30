11a9d986526dbf1c729dac4f5732bcdf8722036f
russik2013 <z.kon2009@yandex.ru>
2016-10-01 01:14:04 +0300

    rusik

--- index.php
+++ index.php
@@ -9,6 +9,22 @@ $output = json_decode(file_get_contents('php://input'),true);
 $id = $output['message']['chat']['id'];
 $message = $output['message']['text'];
 $token = '272967076:AAFnC6WbVpExcWWoSXf1TUTE1WlnRiyKLrQ';
+
+switch ($message){
+
+    case 'hi':
+        $message = 'Hello';
+        sendMessage($token, $id, $message);
+        break;
+    case 'how are you?':
+        $message = 'i am fine';
+        sendMessage($token, $id, $message);
+        break;
+    default:
+        $message = 'What are you say?';
+        sendMessage($token, $id, $message);
+}
+
 sendMessage($token, $id, $message);
 function sendMessage($token, $id, $message)
 {
