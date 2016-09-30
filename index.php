82f6ec10db31d118ee04eb0229b7f15cc2f574c9
russik2013 <z.kon2009@yandex.ru>
2016-10-01 01:06:10 +0300

    rusik

--- index.php
+++ index.php
@@ -7,29 +7,9 @@
  */
 $output = json_decode(file_get_contents('php://input'),true);
 $id = $output['message']['chat']['id'];
-$message = $output['message']['text'];
 $token = '272967076:AAFnC6WbVpExcWWoSXf1TUTE1WlnRiyKLrQ';
-
-switch ($message){
-
-    case'hi':
-        $message = 'Hello';
-        sendMessage($token, $id, $message);
-
-        break;
-
-    case'how are you?':
-        $message = 'I am fine, and you?';
-        sendMessage($token, $id, $message);
-        break;
-    default:
-        $message = 'What are you say?';
-        sendMessage($token, $id, $message);
-}
-
-sendMessage($token, $id, $message);
-function sendMessage($token, $id, $message)
+function sendMessage($token, $id)
 {
-    file_get_contents("https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=" . $id . "&text=".$message);
+    file_get_contents("https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=" . $id . "&text=i can see you");
 }
-//file_put_contents("logs.txt",$id);
+file_put_contents("logs.txt",$id);
