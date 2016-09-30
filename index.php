4c7844520e92e9ac99e18e47efbff8caf71a7799
russik2013 <z.kon2009@yandex.ru>
2016-10-01 00:51:02 +0300

    rusik

--- index.php
+++ index.php
@@ -8,13 +8,8 @@
 $output = json_decode(file_get_contents('php://input'),true);
 $id = $output['message']['chat']['id'];
 $token = '272967076:AAFnC6WbVpExcWWoSXf1TUTE1WlnRiyKLrQ';
-
-sendMessage($id, $token ); 
-
-function sendMessage($token, $id){
-
-    file_get_contents("https://api.telegram.org/bot".$token."/sendMessage?chat_id=".$id."&text=i can see you");
-
+function sendMessage($token, $id)
+{
+    file_get_contents("https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=" . $id . "&text=i can see you");
 }
-
-file_put_contents("logs.txt",$id);
+//file_put_contents("logs.txt",$id);
